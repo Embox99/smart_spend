d0988f7ad0b73491284fb3fef5bc7331718091d8
Eduard Vilensky <embox53@gmail.com>
2025-03-25 18:59:26 +0200

    auth layout/login page

--- frontend/smart_spend/src/components/layouts/AuthLayout.jsx
+++ frontend/smart_spend/src/components/layouts/AuthLayout.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import card from "../../assets/images/card.png";
+import { LuTrendingUpDown } from "react-icons/lu";
 
 const AuthLayout = ({ children }) => {
   return (
@@ -13,12 +14,12 @@ const AuthLayout = ({ children }) => {
         <div className="w-48 h-56 rounded-[40px] border-[20px] border-fuchsia-600 absolute top-[30%] -right-10" />
         <div className="w-48 h-48 rounded-[40px] bg-violet-500 absolute -bottom-7 -left-5" />
 
-        <div className="">
+        <div className="grid grid-cols-1 z-20">
           <StatsInfoCard
-            icon={""}
+            icon={<LuTrendingUpDown />}
             label="Track Yor Income and Expenses"
             value="430,000"
-            color="bg-primary"
+            color="purple"
           />
         </div>
 
@@ -32,3 +33,19 @@ const AuthLayout = ({ children }) => {
 };
 
 export default AuthLayout;
+
+const StatsInfoCard = ({ icon, label, value, color }) => {
+  return (
+    <div className="flex gap-6 bg-white p-4 rounded-xl shadow-md shadow-purple-400/10  border border-gray-200/50 z-100">
+      <div
+        className={`w-12 h-12 flex items-center  justify-center text-[26px] text-white bg-purple-600 rounded-full drop-shadow-xl`}
+      >
+        {icon}
+      </div>
+      <div>
+        <h6 className="text-xs text-gray-500 mb-1">{label}</h6>
+        <span className="text-[20px]">{value}</span>
+      </div>
+    </div>
+  );
+};
