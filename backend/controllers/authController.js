95e195a4368e1ff41a3e8bb826287d1fa8ebe9d4
Eduard Vilensky <embox53@gmail.com>
2025-04-10 23:22:34 +0300

    authentication apis - signup

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,3 +1,5 @@
+const User = require("../models/User");
+const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 
 //generate jwt token
@@ -5,3 +7,45 @@ const jwt = require("jsonwebtoken");
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1h" });
 };
+
+// register user
+
+exports.registerUser = async (req, res) => {
+  const { fullName, email, password, profileImageUrl } = req.body;
+
+  //Validation
+  if (!fullName || !email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  try {
+    //Check if email already exist
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "email is already used" });
+    }
+    //Create user
+
+    const User = await User.create({
+      fullName,
+      email,
+      password,
+      profileImageUrl,
+    });
+
+    res.status(201).json({
+      id: user._id,
+      user,
+      token: generateToken(user._id),
+    });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Error registering user", error: err.message });
+  }
+};
+
+//login user
+exports.loginUser = async (req, res) => {};
+//
+exports.getUserInfo = async (req, res) => {};
