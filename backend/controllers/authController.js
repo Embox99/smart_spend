420fe38e30f2f1244833c81024d214257a8c3112
Eduard Vilensky <embox53@gmail.com>
2025-04-11 21:18:14 +0300

    authentication apis-login, get user

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -46,6 +46,41 @@ exports.registerUser = async (req, res) => {
 };
 
 //login user
-exports.loginUser = async (req, res) => {};
-//
-exports.getUserInfo = async (req, res) => {};
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user || !(await user.comparePassword(password))) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    res
+      .status(200)
+      .json({ id: user._id, user, token: generateToken(user._id) });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Error registering user", error: err.message });
+  }
+};
+
+//get user
+exports.getUserInfo = async (req, res) => {
+  try {
+    const user = User.findById(req.user._id).select("-password");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Error registering user", error: err.message });
+  }
+};
