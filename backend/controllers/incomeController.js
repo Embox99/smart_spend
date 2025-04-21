6f37ffb102d3c81d6e4cbeeebde3c77fae999595
Eduard Vilensky <embox53@gmail.com>
2025-04-21 22:21:32 +0300

    income apis

--- backend/controllers/incomeController.js
+++ backend/controllers/incomeController.js
@@ -1,3 +1,4 @@
+const xlsx = require("xlsx");
 const User = require("../models/User");
 const Income = require("../models/Income");
 
@@ -31,12 +32,47 @@ exports.addIncome = async (req, res) => {
 
 //Get all income source
 
-exports.getAllIncome = async (req, res) => {};
+exports.getAllIncome = async (req, res) => {
+  const userId = req.user.id;
+  try {
+    const income = await Income.find({ userId }).sort({ date: -1 });
+    res.json(income);
+  } catch (err) {
+    res.status(500).json({ message: "Server Error" });
+  }
+};
 
 //Delete income source
 
-exports.deleteIncome = async (req, res) => {};
+exports.deleteIncome = async (req, res) => {
+  try {
+    await Income.findByIdAndDelete(req.params.id);
+    res.json({ message: "Income deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ message: "Server Error" });
+  }
+};
 
 //Download Excel
 
-exports.downloadIncomeExcel = async (req, res) => {};
+exports.downloadIncomeExcel = async (req, res) => {
+  const userId = req.user.id;
+
+  try {
+    const income = Income.find({ userId }).sort({ date: -1 });
+    //Data for Excel
+    const data = income.map((item) => ({
+      Source: item.source,
+      Amount: item.amount,
+      Date: item.date,
+    }));
+
+    const wb = xlsx.utils.book_new();
+    const ws = xlsx.utils.json_to_sheet(data);
+    xlsx.utils.book_apppend_sheet(wb, ws, "Income");
+    xlsx.writeFile(wb, "income_details.xlsx");
+    res.download("income_details.xlsx");
+  } catch (err) {
+    res.status(500).json({ message: "Server Error" });
+  }
+};
