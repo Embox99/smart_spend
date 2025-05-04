e4259e953ded4c5f6ea9350b7991cddd6a16c6b4
Eduard Vilensky <embox53@gmail.com>
2025-05-04 19:05:31 +0300

    dashboard layout - sidemenu

--- frontend/smart_spend/src/components/layouts/SideMenu.jsx
+++ frontend/smart_spend/src/components/layouts/SideMenu.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import { SIDE_MENU_DATA } from "../../utils/data";
+import { UserContext } from "../../context/UserContext";
+import { useNavigate } from "react-router-dom";
 
-const SideMenu = () => {
+const SideMenu = (activeMenu) => {
+  const [user, clearUser] = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const handleClick = (route) => {
+    if (route === "logout") {
+      handleLogout();
+      return;
+    }
+    navigate(route);
+  };
+
+  const handleLogout = () => {
+    localStorage.clear();
+    clearUser();
+    navigate("/login");
+  };
   return (
-    <div>SideMenu</div>
-  )
-}
+    <div>
+      <div>
+        {user?.profileImageUrl ? (
+          <img
+            src={user.profileImageUrl || ""}
+            alt="profile image"
+            className="w-20 h-20 bg-slate-400 rounded-full"
+          />
+        ) : (
+          <></>
+        )}
+        <h5 className="text-gray-950 font-medium leading-6">
+          {user?.fullName || ""}
+        </h5>
+      </div>
+      {SIDE_MENU_DATA.map((item, index) => (
+        <button
+          key={`menu_${index}`}
+          className={`w-full flex items-center gap-4 text-[15px] ${
+            activeMenu == item.label ? "text-white bg-primary" : <></>
+          } py-3 px-6 rounded-lg mb-3`}
+          onClick={() => handleClick(item.path)}
+        >
+          <item.icon className="" />
+          {item.label}
+        </button>
+      ))}
+    </div>
+  );
+};
 
-export default SideMenu
+export default SideMenu;
