ba4dad183aefe150378fd3ab910cdd59e29a0731
Eduard Vilensky <embox53@gmail.com>
2025-05-24 17:11:06 +0300

    expense overview section

--- frontend/smart_spend/src/components/expense/ExpenseOverview.jsx
+++ frontend/smart_spend/src/components/expense/ExpenseOverview.jsx
@@ -1,6 +1,37 @@
 import React from "react";
-
+import { useState, useEffect } from "react";
+import { prepareExpenseLineChartData } from "../../utils/helper";
+import { LuPlus } from "react-icons/lu";
+import { CustomLineChart } from "../charts/CustomLineChart";
 
 export const ExpenseOverview = ({ transactions, onExpenseIncome }) => {
-  return <div>ExpenseOverview</div>;
+  const [charData, setCharData] = useState([]);
+
+  useEffect(() => {
+    const result = prepareExpenseLineChartData(transactions);
+    setCharData(result);
+
+    return () => {};
+  }, [transactions]);
+
+  return (
+    <div className="card">
+      <div className="flex items-center justify-between">
+        <div className="">
+          <h5 className="text-lg">Expense Overview</h5>
+          <p className="text-xs text-gray-400 mt-0.5">
+            Track your spending trends over time and gain insights where your
+            money goes.
+          </p>
+        </div>
+        <button className="add-btn" onClick={onExpenseIncome}>
+          <LuPlus className="text-lg" />
+          Add Expense
+        </button>
+      </div>
+      <div className="mt-10">
+        <CustomLineChart data={charData} />
+      </div>
+    </div>
+  );
 };
